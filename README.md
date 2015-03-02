e8453913c1988672b60f04c9b93daadf14afe00a
islomar <islomar@gmail.com>
2015-03-02 23:55:28 +0100

    Baby steps: accept arguments per command line and print out the sum

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ $ node program.js
 
 $ learnyounode verify program.js
 $ learnyounode run program.js
+
+##Baby steps
+Write a program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout).
--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log("HELLO WORLD");
+var totalSum = 0;
+var numberOfArguments = process.argv.length;
+if (numberOfArguments >= 3) {
+	for (var i=2; i<numberOfArguments; i++) {
+		totalSum += Number(process.argv[i]);
+	}	
+}
+
+console.log(totalSum);
